1bd22b5aadfc649329f9ff70cfe288f77ac5896b
ROKAYASURESH <sureshrokaya761@gmail.com>
2023-05-01 06:38:23 +0545

    My Portfolio bootstrap

---
+++ script.js
@@ -0,0 +1,17 @@
+  // typing text animation script Start...............
+  var typed = new Typed(".typing", {
+    strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+var typed = new Typed(".typing-2", {
+    strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+// typing text animation script  Exit................
+
+  
